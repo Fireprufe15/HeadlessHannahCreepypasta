55e2f009dca3381697550da46a80c19c622a6614
MG Potgieter <mgandr@gmail.com>
2017-12-13 23:25:53 +0200

    Worked on some writing for  the story

--- MainApp/data/story.js
+++ MainApp/data/story.js
@@ -3,33 +3,155 @@ var storyItems = [
         type: "expo",
         message:"In the year 2017, ten teenagers attending a school camp entered a forest. " +
         "Witnesses say that they were terrified of something, running screaming into the forest. " +
-        "They were never seen again. Investigators, while searching for the missing students, were able to find a cellphone " +
-        "that belonged to one of the teens. The phone was scrubbed clean of all data, except for one WhatsApp chat history. " +
-        "This is that chat..."
+        "They were never seen again. Investigators, while searching for the missing students, weren't able to find anything " +
+        "or anyone. One person, who was not at the camp, handed a chat log over to the detectives. She claims that she was " +
+        "talking to one of the teens who went missing. " +
+        "This is that chat log..."
     },
     {
         type: "msg",
-        sender:"Dan",
-        message: "Hey how are you?"
+        sender:"Sarah",
+        message: "Idk what to do help me please"
     },
     {
         type: "msg",
-        sender:"Hannah",
-        message: "Not very good, my head is off"
+        sender:"Lena",
+        message: "Whats up?"
     },
     {
         type: "msg",
-        sender: "Dan",
-        message: "LOL don't joke like that!"
+        sender: "Sarah",
+        message: "You know we are at the camp thing, right?"
     },
     {
         type: "msg",
-        sender:"Hannah",
-        message: "Look behind you..."
+        sender:"Lena",
+        message: "Don't rub it in, it sucks that I'm missing out"
     },
     {
         type: "msg",
-        sender:"Dan",
-        message: "WHAT THE FU"
-    }
+        sender:"Sarah",
+        message: "No there's shit going down"
+    },
+    {
+        type: "msg",
+        sender:"Lena",
+        message: "What kind of shit?"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "Someone was banging on our cabin windows, " +
+        "I couldn't really make the person out but they had this bloody knife"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "So we all just noped out and ran in the opposite direction"
+    },
+    {
+        type: "msg",
+        sender:"Lena",
+        message: "That sounds terrifying, are you safe now?"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "NO"
+    },
+    {
+        type: "msg",
+        sender:"Lena",
+        message: "What does that mean?"
+    },
+    {
+        type: "msg",
+        sender:"Lena",
+        message: "SARAH? Answer me!"
+    },
+    {
+        type: "msg",
+        sender:"Lena",
+        message: "Sarah this isn't funny..."
+    },
+    {
+        type: "msg",
+        sender:"Lena",
+        message: "Dammit don't do this to me!"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "Sorry"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "That thing was close"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "had to run again"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "its some creepy chick in a white sheet"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "theres like blood coming out of her neck"
+    },
+    {
+        type: "msg",
+        sender:"Lena",
+        message: "Sounds like some idiot is pranking you"
+    },
+    {
+        type: "msg",
+        sender:"Lena",
+        message: "you sure it isn't some of the idiot guys?"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "no this is legit shit"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "that wasn't fake blood"
+    },
+    {
+        type: "msg",
+        sender:"Lena",
+        message: "Are you far away now?"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "I think so. we ran into the forest"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "I'm hiding in a bush"
+    },
+    {
+        type: "msg",
+        sender:"Lena",
+        message: "stay put, don't move"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "SHIT"
+    },
+    {
+        type: "msg",
+        sender:"Sarah",
+        message: "i heard a scream...I think Henry is dead."
+    },
 ];
